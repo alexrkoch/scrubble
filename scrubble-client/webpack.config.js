70a6c79a11225724dc40b86a0a0f43649749d244
Mitch Powell <mitchpowell93@gmail.com>
2021-04-12 01:09:34 -0400

    Create webpack configuration and scripts to automate webpack process

---
+++ scrubble-client/webpack.config.js
@@ -0,0 +1,57 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const PathType = Object.freeze({
+  JS: "./src/ts/pages/{}.ts",
+  SCSS: "./src/styles/pages/{}.scss",
+  HTML: "./src/html/{}.html",
+});
+
+const getPagePath = function (pageName, pathType) {
+  return pathType.replace("{}", pageName);
+};
+
+const pages = ["home", "game"];
+
+const entries = pages.reduce(function (acc, val) {
+  return {
+    ...acc,
+    [val]: [getPagePath(val, PathType.JS), getPagePath(val, PathType.SCSS)],
+  };
+}, {});
+
+module.exports = {
+  entry: entries,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    usedExports: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    ...pages.map(
+      (pageName) =>
+        new HtmlWebpackPlugin({
+          template: PathType.HTML.replace("{}", pageName),
+          inject: true,
+          filename: `${pageName}.html`,
+          chunks: [pageName],
+        })
+    ),
+  ],
+};
